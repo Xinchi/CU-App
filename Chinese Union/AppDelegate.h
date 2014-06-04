fcaf46fffaaad1d35a6a70317aaaeb636ac38b67
Max Gu <guxinchi2000@gmail.com>
2014-06-03 18:21:29 -0700

    Initial Commit

---
+++ Chinese Union/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Chinese Union
+//
+//  Created by Max Gu on 6/3/14.
+//  Copyright (c) 2014 ucsd.ChineseUnion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Chinese Union/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Chinese Union
+//
+//  Created by Max Gu on 6/3/14.
+//  Copyright (c) 2014 ucsd.ChineseUnion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
