638e77cb2d17663d260e365a4eb0d8858cf3e2f5
Max Gu <guxinchi2000@gmail.com>
2014-06-09 21:05:35 -0700

    add SignUpViewController

---
+++ Chinese Union/SignUpViewController.m
@@ -0,0 +1,38 @@
+//
+//  SignUpViewController.m
+//  Chinese Union
+//
+//  Created by Max Gu on 6/9/14.
+//  Copyright (c) 2014 ucsd.ChineseUnion. All rights reserved.
+//
+
+#import "SignUpViewController.h"
+
+@interface SignUpViewController ()
+
+@end
+
+@implementation SignUpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
