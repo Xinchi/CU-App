e270f59e931e255fe9275bb339f0896665416447
Max Gu <guxinchi2000@gmail.com>
2014-06-11 21:38:03 -0700

    add signup method

--- Chinese Union/SignUpViewController.m
+++ Chinese Union/SignUpViewController.m
@@ -35,4 +35,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)doSignup {
+    PFUser *user = [PFUser user];
+    user.username = @"my name";
+    user.password = @"my pass";
+    user.email = @"email@example.com";
+    
+    // other fields can be set just like with PFObject
+    user[@"phone"] = @"415-392-0202";
+    user[@"wechat"] = @"";
+    user[@"firstName"] = @"";
+    user[@"lastName"] = @"";
+    user[@"birthday"] = @"";
+    user[@"memberID"] = @"";
+    user[@""] = @"";
+    
+    
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            // Hooray! Let them use the app now.
+        } else {
+            NSString *errorString = [error userInfo][@"error"];
+            // Show the errorString somewhere and let the user try again.
+        }
+    }];
+}
 @end
