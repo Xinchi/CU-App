1f7fc13f99706bac8451a0e5dce96e0bd8679c03
Wei-Ping-Liao <wpliao1989@gmail.com>
2014-06-22 19:49:30 -0700

    Profile UI
    Initial design of profile page UI

--- Chinese Union/ProfileViewController.m
+++ Chinese Union/ProfileViewController.m
@@ -11,6 +11,7 @@
 #import "MySignUpViewController.h"
 #import "TWTSideMenuViewController/TWTSideMenuViewController.h"
 #import "User.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ProfileViewController ()
 
@@ -20,21 +21,57 @@
 @property (weak, nonatomic) IBOutlet UILabel *phoneLabel;
 @property (weak, nonatomic) IBOutlet UILabel *wechatLabel;
 @property (weak, nonatomic) IBOutlet UILabel *birthdayLabel;
-@property (weak, nonatomic) IBOutlet UIImage *profilePic;
+@property (weak, nonatomic) IBOutlet UIImageView *profilePicView;
+
+@property (strong, nonatomic) UIActionSheet *actionSheet;
+@property (strong, nonatomic) NSDateFormatter *dateFormatter;
 
 @end
 
 @implementation ProfileViewController
 
+- (NSDateFormatter *)dateFormatter {
+    if (_dateFormatter == nil) {
+        _dateFormatter = [[NSDateFormatter alloc] init];
+    }
+    return _dateFormatter;
+}
+
+- (UIActionSheet *)actionSheet {
+    if (_actionSheet == nil) {
+        _actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:@"Log out" otherButtonTitles: nil];
+    }
+    return _actionSheet;
+}
 
 #pragma mark - UIViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"Blur_background"]];
+    
+    self.profilePicView.layer.cornerRadius = 8;
+    self.profilePicView.layer.masksToBounds = YES;
+    self.profilePicView.layer.borderWidth = 0;
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
 - (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
     User *user = [User currentUser];
     
     if (user) {
-        self.userNameLabel.text = [NSString stringWithFormat:NSLocalizedString(@"Welcome %@!", nil), user.username];
+        self.userNameLabel.text = [NSString stringWithFormat:@"%@", user.username];
         self.realNameLabel.text = [NSString stringWithFormat:@"%@", user.firstName];
+        self.emailLabel.text    = [NSString stringWithFormat:@"%@", user.email];
+        self.birthdayLabel.text = [NSString stringWithFormat:@"%@", [self.dateFormatter stringFromDate:user.birthday]];
+        self.phoneLabel.text    = [NSString stringWithFormat:@"%@", user.phone];
+        self.wechatLabel.text   = [NSString stringWithFormat:@"%@", user.wechatID];
     } else {
         self.userNameLabel.text = NSLocalizedString(@"Not logged in", nil);
     }
@@ -42,14 +79,20 @@
     NSLog(@"objectId = %@", user.objectId);
 }
 
+#pragma mark - UIActionSheet delegate
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == self.actionSheet.destructiveButtonIndex) {
+        [User logOut];
+        [self.navigationController popViewControllerAnimated:YES];
+        [self.sideMenuViewController closeMenuAnimated:YES completion:nil];
+    }
+}
+
 #pragma mark - IBAction
 
 - (IBAction)logOutButtonTapAction:(id)sender {
-    [User logOut];
-    [self.navigationController popViewControllerAnimated:YES];
-    [self.sideMenuViewController closeMenuAnimated:YES completion:nil];
-    
+    [self.actionSheet showInView:self.view];
 }
-
 
 @end
